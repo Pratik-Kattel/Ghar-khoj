bc7be4f93b39d8d5058ed10e282dda643bd28e8c
Pratik-Kattel <kattelpratik007@gmail.com>
2026-03-11 16:30:33 +0545

    Modified validateEmailService to store the otp into database

--- GharKhoj-backend/src/service/password_change_service.ts
+++ GharKhoj-backend/src/service/password_change_service.ts
@@ -1,5 +1,6 @@
 import { email } from "zod";
 import { userData } from "../types/user";
+import cuid from 'cuid';
 import pool from "../config/db";
 import logger from "../config/logger";
 import { sendEmail } from "../utils/mailer";
@@ -12,7 +13,12 @@ export const validateEmailService=async({email}:userData)=>{
         throw new Error("User with this email address doesn't exists");
     }
     const username=emailValidate.rows[0].name;
+    const user_id=emailValidate.rows[0].user_id;
     const otp=Math.floor(100000+Math.random()*900000);
     await sendEmail(email,otp,username);
+    const expires_at=new Date(Date.now()+5*60*1000);
+    const otp_id=cuid();
+    const expiry=await pool.query("Insert into otp_logs(otp_id,user_id,otp_code,purpose,expires_at) VALUES ($1,$2,$3,$4,$5) RETURNING *",[otp_id,user_id,otp,"Password_change",expires_at]);
+
 
 }
